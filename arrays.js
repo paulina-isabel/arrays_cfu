00efe94ca0b394323a78eb91c6d2423cce1807b6
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-02-09 03:20:11 -0600

    add calls to array methods and explanations

--- arrays.js
+++ arrays.js
@@ -1,3 +1,10 @@
 var plants = ["Monstera", "Pothos", "Fiddle Leaf Fig", "ZZ Plant"];
+//in this call I'm using the pop method which will exclude the last element of this array//
+plants.pop ();
+console.log(plants);
+
 var merchQuantity = [6, 12, 24, 72];
-var bookReads = [true, true, false, false,'];
+//in this call I'm using the push method which will add an element to the end of this array//
+merchQuantity.push (100);
+console.log(merchQuantity);
+
